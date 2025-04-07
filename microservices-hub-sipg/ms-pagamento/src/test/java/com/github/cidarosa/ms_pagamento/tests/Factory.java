c30cefde9babc4b0ac079d51e3399c56d4b0ff16
RafaMiura <143235499+RafaMiura@users.noreply.github.com>
2025-04-07 11:29:24 -0300

    Add files via upload

---
+++ microservices-hub-sipg/ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/tests/Factory.java
@@ -0,0 +1,33 @@
+package com.github.cidarosa.ms_pagamento.tests;
+
+
+
+import com.github.cidarosa.ms_pagamento.dto.PagamentoDTO;
+import com.github.cidarosa.ms_pagamento.entity.Pagamento;
+import com.github.cidarosa.ms_pagamento.entity.Status;
+
+import java.math.BigDecimal;
+
+public class Factory {
+
+    public static Pagamento createPagamento(){
+        Pagamento pagamento = new Pagamento(1L, BigDecimal.valueOf(32.25),
+                "Jon Snow", "2345789632145789",
+                "07/32", "585", Status.CRIADO, 1L, 2L);
+
+        return  pagamento;
+    }
+
+    public static PagamentoDTO createPagamentoDTO(){
+        Pagamento pagamento = createPagamento();
+        return new PagamentoDTO(pagamento);
+    }
+
+    public static PagamentoDTO createNewPagamentoDTO(){
+
+        Pagamento pagamento = createPagamento();
+        pagamento.setId(null);
+        return new PagamentoDTO(pagamento);
+    }
+}
+
