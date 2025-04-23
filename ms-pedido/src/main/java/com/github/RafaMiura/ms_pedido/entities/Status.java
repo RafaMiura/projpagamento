9fe0627d8ea3c69fefd073c6c5465e5d8f61142c
RafaMiura <143235499+RafaMiura@users.noreply.github.com>
2025-04-23 09:45:35 -0300

    Add files via upload

---
+++ ms-pedido/src/main/java/com/github/RafaMiura/ms_pedido/entities/Status.java
@@ -0,0 +1,14 @@
+package com.github.RafaMiura.ms_pedido.entities;
+
+public enum Status {
+    REALIZADO,
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    CONFIRMAOD,
+    PRONTO_PARA_ENTREGA,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE,
+    CANCELADO
+
+
+}
